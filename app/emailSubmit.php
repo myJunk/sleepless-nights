c5cf5451e408ba2dabc19889baca2f6cea95e97e
OFam <OFam@OFam.local>
2016-02-01 19:44:42 -0800

    initial commit

---
+++ app/emailSubmit.php
@@ -0,0 +1,112 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    $email_to = "admin@sleepless-nights-253.com";
+ 
+    $email_subject = "Coming Soon - Notification";
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+   
+ 
+ 
+    $email_from = $_POST['email']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     $email_message .= "Email: ".clean_string($email_from)."\n Needs to be added to the notification list";
+ 
+   
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+  
+ 
+    $sent = @mail($email_to, $email_subject, $email_message, $headers);
+
+    if ($sent) {
+      header("Location: index.html");
+      exit();
+    } else {
+      print "We encountered an error sending your mail"; 
+    }
+    
+    
+?>
+
+
+
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ 
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
